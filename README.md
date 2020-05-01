05935d78fb45faaff731e0922e9b6aeff2d36b36
Harry Turnbull <hturnbull@live.co.uk>
2020-05-01 12:23:49 +0100

    wrote one task at a time example

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # JavaScript Multi Tasking Example
 
 A basic program to illustrate ways of multi tasking in JavaScript.
+
+Todo List
+
+- Grocery Shopping
+  1. Go to the shops
+  2. Pick out your items
+  3. Pay for your items
+  4. Return home
+  5. Put shopping in fridge
+- Receive Fridge Delivery
+  1. Wait for Curry's delivery
+  2. Sign for fridge
+  3. Moves fridge to kitchen
+  4. Unpack fridge
+  5. Plug fridge in
---
+++ oneTaskAtATime.js
@@ -0,0 +1,64 @@
+// Some variables to handle
+
+var shops = {
+  items: ["ham", "eggs", "spam"]
+}
+
+var home = {
+  fridge: null
+}
+
+var delivery = {
+  fridge: {
+    contents: [],
+    installed: false
+  }
+}
+
+// A function that takes some time, and blocks the main thread
+
+function takeTime(length) {
+  let seconds = length * 1000000000
+  let start = new Date()
+  for (let i = 0; i < seconds; i++) {}
+  let end = new Date()
+  let timeTaken = end - start
+  console.log(`That took: ${timeTaken} ms`)
+}
+
+// Functions that take some time
+
+function goToShops() {
+  console.log("Going to shops")
+  takeTime(1)
+}
+
+function goHome() {
+  console.log("Finished shopping, going home")
+  takeTime(1)
+}
+
+function waitForDelivery() {
+  console.log("Waiting for delivery")
+  takeTime(5)
+}
+
+// Functions representing items on the todo list
+
+function groceryShopping() {
+  goToShops()
+  let shoppingBags = [...shops.items]
+  goHome()
+  home.fridge.contents = [...shoppingBags]
+  console.log("Back home now, shopping is in the fridge")
+}
+
+function receiveFridge() {
+  waitForDelivery()
+  home.fridge = delivery.fridge
+  home.fridge.installed = true
+  console.log("Installed fridge")
+}
+
+groceryShopping()
+receiveFridge()
