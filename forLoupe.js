ee2799f97db54e42c33b95979c770806c1976dcb
Harry Turnbull <hturnbull@live.co.uk>
2020-05-01 14:04:00 +0100

    added code that loupe should be able to use

---
+++ forLoupe.js
@@ -0,0 +1,34 @@
+var shops = {
+  items: ["ham", "eggs", "spam"]
+};
+
+var home = {
+  fridge: null
+};
+
+var delivery = {
+  fridge: {
+    contents: [],
+    installed: false
+  }
+};
+
+function groceryShopping() {
+  console.log("Going to shops")
+  let shoppingBags = [...shops.items];
+  console.log("Going home")
+  setTimeout(() => {
+    home.fridge.contents = [...shoppingBags];
+    console.log("Shopping is in the fridge");
+  }, 0);
+}
+
+function receiveFridge() {
+  console.log("Waiting for delivery")
+  home.fridge = delivery.fridge;
+  home.fridge.installed = true;
+  console.log("Installed fridge");
+}
+
+groceryShopping();
+receiveFridge();
